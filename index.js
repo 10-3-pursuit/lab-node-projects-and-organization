c3e472ed3c4b28db23c4503ce34d2df65701f198
juligarc91 <julissagarcia@pursuit.org>
2023-10-04 15:33:11 -0400

    Completed Fx firstTenWords

--- index.js
+++ index.js
@@ -10,4 +10,28 @@ function allWords(wordsArr) {
     }
     return loggedWords;
 };
-console.log(allWords(importedData));
+//console.log(allWords(importedData));
+
+function firstTenWords (wordsArr) {
+    let loggedFirst10Words = "";
+    let word = "";
+    for (let i=0; i < 10; i++) {
+        word = wordsArr[i];
+        loggedFirst10Words += `${word} `;
+        loggedFirst10Words.trim();
+    }
+    return loggedFirst10Words;
+};
+//console.log (firstTenWords(importedData));
+
+function firstTenWords (wordsArr) {
+    let loggedNext10Words = "";
+    let word = "";
+    for (let i=9; i < 20; i++) {
+        word = wordsArr[i];
+        loggedNext10Words += `${word} `;
+        loggedNext10Words.trim();
+    }
+    return loggedNext10Words;
+};
+console.log (firstTenWords(importedData));
