7f0d2ed733da8aa267342e39d1de48af72bb8c7e
AnitaOwen <anitaowen@pursuit.org>
2023-10-04 16:11:14 -0400

    completed next 2 functions

--- index.js
+++ index.js
@@ -44,3 +44,33 @@ function sortWords(words){
     return words.sort((a, b) => a.localeCompare(b))
 }
 console.log(sortWords(wordsJSON))
+
+
+// returns all the words that contain the letter q
+function wordsWithQ(words){
+    const qWords = words.filter((word) => word.includes("q"))
+    if(qWords.length === 0){
+        return "There are no words with the letter Q"
+    } else {
+        return qWords
+    }
+}
+console.log(wordsWithQ(wordsJSON))
+
+
+//  takes an argument letter and returns all the words with that matching letter
+function findWordsWithLetter(words, letter){
+    letter = letter.toLowerCase()
+    const letterMatches = words.filter((word) => word.includes(letter))
+    if(letterMatches === 0){
+        return `There are no words with the letter ${letter}`
+    } else {
+        return letterMatches
+    }
+}
+console.log(findWordsWithLetter(wordsJSON, "z"))
+console.log(findWordsWithLetter(wordsJSON, "M"))
+
+
+
+
