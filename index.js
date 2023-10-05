c94141a26ae4b9a575dc122b00924d37a009e0bc
brenda-soto <bsoto@pursuit.org>
2023-10-05 09:45:48 -0400

    Completed a couple of bonus functions

--- index.js
+++ index.js
@@ -32,3 +32,38 @@ const subsetOfWords = (words, start, end) => wordLogger(words, start, end)
 const sortWords = words => wordLogger(words.sort())
 
 // sortWords(wordsJSON)
+
+
+const wordsWithQ = words => wordLogger(words.filter(word => word.includes('q')))
+
+// wordsWithQ(wordsJSON)
+
+
+const findWordsWithLetter = (words, letter) => wordLogger(words.filter(word => word.includes(letter)))
+
+// findWordsWithLetter(wordsJSON, process.argv[2])
+
+
+// BONUS~~~~~~~~~~~~~~~
+const letterMatch = (words, letters) => {
+    if(letters.length <= 5) {
+        wordLogger(words.filter(word => {
+            return letters.split('').map(letter => word.includes(letter)).includes(true)
+        }))
+    } else {
+        console.log('You must input 5 or less letters.')
+    }
+}
+
+// letterMatch(wordsJSON, process.argv[2])
+
+
+const letterExactMatch = (words, letters) => {
+    if(letters.length === 5) {
+        wordLogger(words.filter(word => !letters.split('').map((letter, index) => word[index] === letter || letter === '.' || letter === '_').includes(false)))
+    } else {
+        console.log('You must input exactly 5 letters')
+    }
+}
+
+// letterExactMatch(wordsJSON, process.argv[2])
