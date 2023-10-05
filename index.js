294dab2e14d45e9e83a5bd047ef7194ae9390ba1
enochtagoe <enochtagoe@pursuit.org>
2023-10-05 09:46:21 -0400

    completed lab

--- index.js
+++ index.js
@@ -10,10 +10,33 @@ function allWords(arr) {
 // }
    } 
 }
-allWords(wordsJson)
+//allWords(wordsJson)
 
 function firstTenWords(arr) {
-for(let word of arr) {
-console.log(arr)
+    for( let i = 0; i < 10; i++){
+        console.log(arr[i])
+//for(let word of arr) {
+//console.log(arr.map())
 }
 }
+//console.log(firstTenWords(wordsJson))
+function nextTenWords(arr) {
+    for( let i = 10; i < 20; i++){
+        console.log(arr[i])  
+    }
+}
+//console.log(nextTenWords(wordsJson))
+function firstXWords(words, x) {
+    for( let i = 0; i < x; i++) {
+        console.log(words[i])    
+    }
+}
+//console.log(firstXWords(wordsJson, 9))
+function subsetOfWords(words, x, y) {
+let min = Math.min(x, y)
+let max = Math.max(x, y)
+for( let i = min - 1; i < max; i++) {
+    console.log(words[i]) 
+}
+}
+console.log(subsetOfWords(wordsJson,25 , 5))
