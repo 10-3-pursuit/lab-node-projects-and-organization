faa12396f9e1e22fccbf521e1ab9c049b08c1fb3
juligarc91 <julissagarcia@pursuit.org>
2023-10-04 22:37:30 -0400

    Edited fx errors

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const importedData = require ("./words.json");
 
 //console.log(importedData);
 
-//Check if all edge cases are accounted for
 /**
  * Returns a string of all of the words in array input
  * @param {String[]}  - An array of strings.
@@ -14,28 +13,36 @@ function allWords(wordsArr) {
         loggedWords += `${word} `;
         loggedWords.trim();
     }
-    return loggedWords;
+    return console.log(loggedWords); // logs all the words when allWords is invoked
 };
-console.log(allWords(importedData));
+//allWords(importedData); // there is no test file
+
 /**
  * Returns a string of first 10 words in array input
  * @param {String[]}  - An array of strings.
  * @returns {string} - A string of words.
  */
- 
 function firstTenWords (wordsArr) {
+    if (wordsArr.length >= 10) {
     let loggedFirst10Words = "";
     let word = "";
     for (let i=0; i < 10; i++) {
         word = wordsArr[i];
         loggedFirst10Words += `${word} `;
         loggedFirst10Words.trim();
-    }
-    return loggedFirst10Words;
+    }}
+    return console.log(loggedFirst10Words); // logs first ten words when firstTenWords is invoked
 };
-console.log (firstTenWords(importedData));
+//firstTenWords(importedData);
 
+/**
+ * Returns a string of next 10 words in array input starting from the 10th word in an array
+ * @param {String[]}  - An array of strings.
+ * @returns {string} - A string of words.
+ */
 function nextTenWords (wordsArr) {
+    //array must contain at least 20 strings
+    if (wordsArr.length >= 20) {
     let loggedNext10Words = "";
     let word = "";
     for (let i=10; i < 20; i++) {
@@ -43,13 +50,15 @@ function nextTenWords (wordsArr) {
         loggedNext10Words += `${word} `;
         loggedNext10Words.trim();
     }
-    return loggedNext10Words;
+}
+    return console.log(loggedNext10Words); // logs next ten words when nextTenWords is invoked
 };
-console.log (nextTenWords(importedData));
+//nextTenWords(importedData)
 
 // x is an integer number bigger than 0
 
 function firstXWords (wordsArr, x) {
+    if (wordsArr.length >= 2) {
     let loggedXWords = "";
     let word = "";
     if (x > 0) {
@@ -58,14 +67,15 @@ function firstXWords (wordsArr, x) {
         loggedXWords += `${word} `;
         loggedXWords.trim();
     }
-}
+}}
     return loggedXWords;
 };
-console.log (firstXWords(importedData, 5));
+//console.log (firstXWords(importedData, 5));
 
 //x is start (not inclusive)
 //y is end (inclusive)
 function subsetOfWords (wordsArr, x, y) {
+    if (wordsArr.length >= 2) {
     let loggedsubsetOfWords = "";
     let word = "";
     if (x => 0 && y >= 0 && x < y && x !== y) {
@@ -75,14 +85,14 @@ function subsetOfWords (wordsArr, x, y) {
         loggedsubsetOfWords.trim();
     }
 } else {
-    return `Error, must have correct inputs; x can not be greater than or equal to y.`
-}
-    return loggedsubsetOfWords;
+    return undefined;
+}}
+    return console.log(loggedsubsetOfWords);
 };
-//console.log (subsetOfWords(importedData,0,6)); // Output includes words 1 - 6
-//console.log (subsetOfWords(importedData,7,6)); // output is error message
-//console.log (subsetOfWords(importedData,6,6)); // output is error message
-//console.log (subsetOfWords(importedData,4,6)); // output will not include word 4. Output will be words 5 and 6
+//subsetOfWords(importedData,0,6) // Output includes words 1 - 6
+//subsetOfWords(importedData,7,6) // output is error message
+//subsetOfWords(importedData,6,6) // output is error message
+//subsetOfWords(importedData,4,6) // output will not include word 4. Output will be words 5 and 6
 function sortWords (wordsArr) {
     wordsArr.sort();
     let sortedWords = "";
@@ -92,20 +102,34 @@ function sortWords (wordsArr) {
     }
     return sortedWords;
 };
-//console.log (sortWords(importedData));
+//sortWords(importedData)
 
 // (>^_^)> ---- Wordle Solver Functions: ---- <(^_^<)
-
+const testData =["women",
+"nikau",
+"swack",
+"feens",
+"fyles",
+"poled",
+"clags",
+"starn",
+"bindi",
+"woops"
+];
 // fx wordsWithQ Returns list of words that contain letter q
 // finds a word with matching letter q
 // finds a word with more than 1 matching letter q
 // finds a word with no matching letters? probably to return an error
 // not complete because needs edge case for error return 
 function wordsWithQ (wordsArr) {
-   return wordsArr.filter(words => words.includes('q'));
+    const wordsWithQArr = wordsArr.filter(word => word.includes('q'));
+    if (wordsWithQArr.length === 0) {
+        return console.log(wordsArr);
+    }
+    return console.log(wordsWithQArr);
 };
-//console.log(wordsWithQ(importedData));
-
+//wordsWithQ(importedData)
+//wordsWithQ(testData)
 // I think complete? it has the edge case for error return
 function findWordsWithLetter (wordsArr, letter) {
     if (typeof letter === 'string' && letter.length === 1) {
@@ -118,18 +142,10 @@ function findWordsWithLetter (wordsArr, letter) {
         return wordsWithLetterArr.join(" ").trim();
     }
  };
+//findWordsWithLetter(importedData)
+//findWordsWithLetter(testData)
 // ---- Test data: ----
-const testData =["women",
-"nikau",
-"swack",
-"feens",
-"fyles",
-"poled",
-"clags",
-"starn",
-"bindi",
-"woops"
-];
-console.log (findWordsWithLetter(testData, "X")); // returns all words since x isn't included in any of them
+
+//console.log (findWordsWithLetter(testData, "X")); // returns all words since x isn't included in any of them
 //console.log (findWordsWithLetter(testData, "L")); // returns only words with letter L
 //console.log (findWordsWithLetter(importedData, "L")); // should print out words that contain that letter
